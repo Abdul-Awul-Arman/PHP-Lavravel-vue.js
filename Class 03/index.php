73605cd6daa7c5c54fdb451a8b6f7a68f2c2c927
arman52114 <129601164+arman52114@users.noreply.github.com>
2024-07-09 16:19:06 +0600

    learn variable

--- Class 03/index.php
+++ Class 03/index.php
@@ -1,5 +1,15 @@
 <?php
 // echo"abdulawularman";
-$name="abdulawulalrman";
-echo $name;
+// $name="abdulawulalrman";
+// echo $name;
+// $arry=["arman","abb"];
+// print_r($arry);
+
+define("site_name","arman");
+define("constd","the var");
+
+printf(constd."<br>");
+
+echo site_name;
+
 ?>
