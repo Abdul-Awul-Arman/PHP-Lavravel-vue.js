d3d313654ca2bd5e6742d5b5cc274440c1dd07e3
arman52114 <129601164+arman52114@users.noreply.github.com>
2024-08-18 14:11:51 +0600

    learning function

---
+++ Module 05/functon.php
@@ -0,0 +1,36 @@
+<?php
+
+declare(strict_types=1);
+
+//normal fun
+// function fun($one, $tow){
+//     printf("%d",$one+$tow);
+// }
+
+// fun(1,2);
+
+//type hinting
+// function fun(string| null | int $one, int | null | string $tow){
+//     printf("%d",$one+$tow);
+// }
+
+// fun(null,null);
+
+// varidic function
+// function fun(...$var)
+// {
+//     print_r($var);
+// }
+
+// fun("arman", "nur");
+
+    // anonymous funcatin
+    // (function(){
+    //     printf("form anonymous function");
+    // })();
+
+
+    //multiple return type
+    // function demo():int | string{
+    //     return "this";
+    // }
