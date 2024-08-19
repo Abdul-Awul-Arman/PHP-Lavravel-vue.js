40fbd51b13515f294893432bf6e5529393de9606
arman52114 <129601164+arman52114@users.noreply.github.com>
2024-08-19 14:32:50 +0600

    learn array

---
+++ Module 06/Class2.php
@@ -0,0 +1,43 @@
+<?php
+
+// $var = "arman";
+
+// function fun()
+// {
+//     // global variable
+//     global $var;
+//     echo $var;
+// }
+
+// fun();
+
+
+
+//static function variable
+// function fun(){
+//     static $tem;
+//     return $tem++;
+
+// }
+
+
+//     for($i=0;$i<10;$i++){
+//         echo fun();
+//     }
+
+
+
+//default parameter in function
+
+    // function fun($to,$form,$sub="no-reply.com"){
+    //     echo "$to - $form - $sub";
+    // }
+
+    // fun("abc@gmail.com","xyz@gmail.com");
+
+    //named parameter
+    // fun(to:"abc@gmail.com",form:"xyz@gmail.com");
+
+    
+
+?>
---
+++ Module 7/array.php
@@ -0,0 +1,25 @@
+<?php
+
+    // $arry=array("arman","nur","shad","rahat");
+
+    // foreach($arry as $item){
+    //     echo "$item ";
+    // }
+
+
+  //Associative array
+
+    // $arman=[
+    //     "name"=>"arman",
+    //     "roll"=>"50",
+    // ];
+
+    // // print_r($arman["name"]);
+
+    // foreach($arman as $key=>$value){
+    //     echo $value;
+    // }
+
+    
+
+?>
