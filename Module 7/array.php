01ff129a416daf7b45e7d62b9716d24003baf502
arman52114 <129601164+arman52114@users.noreply.github.com>
2024-08-31 15:48:37 +0600

    learn array

--- Module 7/array.php
+++ Module 7/array.php
@@ -1,25 +1,40 @@
 <?php
 
-    // $arry=array("arman","nur","shad","rahat");
+// $arry=array("arman","nur","shad","rahat");
 
-    // foreach($arry as $item){
-    //     echo "$item ";
-    // }
+// foreach($arry as $item){
+//     echo "$item ";
+// }
 
 
-  //Associative array
+//Associative array
 
-    // $arman=[
-    //     "name"=>"arman",
-    //     "roll"=>"50",
-    // ];
+// $arman=[
+//     "name"=>"arman",
+//     "roll"=>"50",
+// ];
 
-    // // print_r($arman["name"]);
+// // print_r($arman["name"]);
 
-    // foreach($arman as $key=>$value){
-    //     echo $value;
-    // }
+// foreach($arman as $key=>$value){
+//     echo $value;
+// }
 
-    
+$arman = ["age" => 22, "name" => "arman", "city" => "sylhet"];
 
-?>
+// $values = array_values($arman);
+// $values = array_keys($arman);
+
+// $ans= array_key_exists("age",$arman);
+// $ans= array_search("arman",$arman);
+
+// $ans=array_flip($arman);
+$ans=asort($arman);
+
+
+ 
+print_r($ans) ;
+
+// foreach ($values as $ans) {
+//   echo "$ans ";
+// }
