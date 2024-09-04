05c85b40e7e3d1473f5d8a2ef3557f1537be6899
arman52114 <129601164+arman52114@users.noreply.github.com>
2024-09-04 18:53:09 +0600

    learn arry

--- Module 7/associative_array_data_extreting.php
+++ Module 7/associative_array_data_extreting.php
@@ -1,15 +1,11 @@
 <?php
 
+$arman = ["age" => 20, "name" => "armna",20=>5, "city" => "sylhet", "father" => true, "mother" => true,"brother"=>true];
 
+// var_dump($arman);
 
+// $demo = array_slice($arman,0,null,true);
 
+print_r($demo);
 
-
-
-// $arman = ["age" => 20, "name" => "armna", "city" => "sylhet"];
-
-// // var_dump($arman);
-
-// $demo=array_slice($arman,1);
-
-// var_dump($demo);
+// echo count($arman);
