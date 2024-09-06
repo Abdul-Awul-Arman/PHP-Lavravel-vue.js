5bcbae495231ae493dfab6f8018e6784e5af1f1a
arman52114 <129601164+arman52114@users.noreply.github.com>
2024-09-06 17:54:10 +0600

    concatenating_array

---
+++ Module 7/Concatenating _array.php
@@ -0,0 +1,4 @@
+<?php
+
+$arry = ["arman", "arafat", "mashrafe", "nur", "rahat", "ahmad", "shad"];
+$arry = ["1" => "arman", "2" => "arafat", "3" => "mashrafe", "4" => "nur", "5" => "rahat", "6" => "ahmad", "7" => "shad"];
